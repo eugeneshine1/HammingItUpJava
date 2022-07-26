14523d7a7c5bed0cc7e469c7e4b900e7624870ef
eugeneshine1 <eugeneshine1@gmail.com>
2022-07-25 23:43:49 -0400

    done hamming

--- src/main/zipcode/Hamming.java
+++ src/main/zipcode/Hamming.java
@@ -1,10 +1,24 @@
-package zipcode;
+package main.zipcode;
 
 public class Hamming {
+    private String s;
+    private String s1;
     public Hamming(String s, String s1) {
+        this.s = s;
+        this.s1 = s1;
+        if (s.length()!=s1.length()) {
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+        }
     }
 
     public int getHammingDistance() {
-        return -1;
+        int hamming = 0;
+        for (int x = 0; x<s.length();x++) {
+            //NOT s.substring(x) bc IDK
+            if (s.charAt(x)!=s1.charAt(x)) {
+                hamming++;
+            }
+        }
+        return hamming;
     }
 }
